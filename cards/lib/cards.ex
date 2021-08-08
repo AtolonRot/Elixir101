26688a04dbca64904af80b0e6341c8124086a45a
Breyner Rojas <mon2bdr@gmail.com>
2021-08-07 20:01:00 -0500

    Create method to save files

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,5 +1,37 @@
 defmodule Cards do
-  def hello do
-    "hi there!"
+  def create_deck do
+    values = ["1", "2", "3"]
+    suits = ["Clubs", "Hearts", "Diamonds"]
+
+    # cards =
+    #   for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    #   end
+    #List.flatten(cards)
+
+    # This approach is like the above
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
   end
 end
