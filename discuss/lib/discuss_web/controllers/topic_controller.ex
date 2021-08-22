c903d3f763213b307810a21bd2bad9c0beaa71d5
Breyner Rojas <brojas22@outlook.com>
2021-08-21 19:13:26 -0500

    Create topic form and view

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,10 +1,13 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  alias Discuss.Topic
 
   def new(conn, params) do
-    IO.puts "+++++"
-    IO.inspect(conn)
-    IO.puts "+++++"
+    changeset = Topic.changeset(%Topic{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, params) do
     IO.inspect(params)
   end
 end
